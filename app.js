b54f8283c976cbd75421249a7ccc3c7bdad538e2
paublas <paublas98@gmail.com>
2022-06-11 11:50:30 +0200

    add simple word check when enter is clicked

--- app.js
+++ app.js
@@ -2,22 +2,37 @@
 words = words.sort()
 
 const cells = document.getElementsByClassName("cell-text")
+const cellSquares = document.getElementsByClassName("cell")
 
 let pointer = 0;
 
-let board = [
-    [undefined, undefined, undefined, undefined, undefined],
-    [undefined, undefined, undefined, undefined, undefined],
-    [undefined, undefined, undefined, undefined, undefined],
-    [undefined, undefined, undefined, undefined, undefined],
-    [undefined, undefined, undefined, undefined, undefined],
-    [undefined, undefined, undefined, undefined, undefined]
-];
+word = 'magic'
 
 document.addEventListener('click', (event)=>{
 
-    console.log(event, event.target.id)
-    cells[pointer].innerHTML = event.target.id
-    pointer++;
+    if(event.target.id != ''){
+        if(event.target.id == 'back'){
+            pointer--;
+            cells[pointer].innerHTML = ''
+            console.log(cells[pointer].innerHTML)
+
+        }
+        else if(event.target.id == 'enter'){
+            if(pointer % 5 == 0){
+                for(let i = 0; i < 5; i++){
+                    if(word[i] == cells[pointer - 5 + i].innerHTML) cellSquares[pointer - 5 + i].style.backgroundColor = "#6aaa64";
+                    else if(word.includes(cells[pointer - 5 + i].innerHTML)) cellSquares[pointer - 5 + i].style.backgroundColor = "#c9b458";
+                    else cellSquares[pointer - 5 + i].style.backgroundColor = "#787c7e";
+                    cellSquares[pointer - 5 + i].style.color = "white";
+                }
+            }
+        }
+        else{
+            console.log(cells[pointer].innerHTML)
+            cells[pointer].innerHTML = event.target.id
+            pointer++;        
+        }
+    }
+
 })
 
