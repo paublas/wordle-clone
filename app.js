ce9216707213346e85921c5ff358b3ea6311f729
paublas <paublas98@gmail.com>
2022-06-11 18:40:07 +0200

    add some features

--- app.js
+++ app.js
@@ -1,24 +1,53 @@
 
-words = words.sort()
+const difference = new Date() - new Date('01/01/2022');
+const days = Math.ceil(difference / (1000 * 3600 * 24));
+
+
+const word = words[days]
+
+
 
 const cells = document.getElementsByClassName("cell-text")
 const cellSquares = document.getElementsByClassName("cell")
 
 let pointer = 0;
 let pointer_limit = 0;
-
-word = 'magic'
+let green_cells = 0
 
 
 function print_cell(i) {
     setTimeout(() => {
         cellSquares[pointer - 5 + i].style.animation = "flip 0.5s linear";
-        if(word[i] == cells[pointer - 5 + i].innerHTML) cellSquares[pointer - 5 + i].style.backgroundColor = "#6aaa64";
+        if(word[i] == cells[pointer - 5 + i].innerHTML) {
+            cellSquares[pointer - 5 + i].style.backgroundColor = "#6aaa64";  green_cells++;
+        }
         else if(word.includes(cells[pointer - 5 + i].innerHTML)) cellSquares[pointer - 5 + i].style.backgroundColor = "#c9b458";
         else cellSquares[pointer - 5 + i].style.backgroundColor = "#787c7e";
-        cellSquares[pointer - 5 + i].style.color = "white";             
+        cellSquares[pointer - 5 + i].style.color = "white";         
+        
+        if(green_cells >= 5) win()
+
     }, 400 * i);
 
+}
+
+function win(){
+    console.log('win') // GAME OVER LOGIC
+}
+
+function wordExists(){
+    console.log(words)
+    if( words.includes(cells[pointer - 5].innerHTML + cells[pointer - 4].innerHTML + cells[pointer - 3].innerHTML 
+        + cells[pointer - 2].innerHTML + cells[pointer - 1].innerHTML ) ) return true;
+    else{
+        //popup 'Not a word list'
+        cellSquares[pointer - 5].style.animation = 'horizontal-shaking 0.4s'
+        cellSquares[pointer - 4].style.animation = 'horizontal-shaking 0.4s'
+        cellSquares[pointer - 3].style.animation = 'horizontal-shaking 0.4s'
+        cellSquares[pointer - 2].style.animation = 'horizontal-shaking 0.4s'
+        cellSquares[pointer - 1].style.animation = 'horizontal-shaking 0.4s'
+        return false;
+    }
 }
 
 document.addEventListener('click', (event)=>{
@@ -32,7 +61,9 @@ document.addEventListener('click', (event)=>{
             }
         }
         else if(event.target.id == 'enter'){
-            if(pointer % 5 == 0){
+            
+            green_cells = 0
+            if(pointer % 5 == 0 && wordExists()){
                 for(let i = 0; i < 5; i++){
                     print_cell(i);
                 }
